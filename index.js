5bcd6a4ca30b674c810d2ca8fbcb18f756d2d390
Coop <coop.coding@gmail.com>
2020-06-09 13:01:39 +1000

    Fixed port and cron

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const {CronJob} = require('cron')
 const {updateFeed} = require('./updateFeed.js')
 
 const app = express()
-const port = 8044
+const port = 80
 const feedXMLFilePath = path.join(__dirname, 'feed.xml')
 const indexHTMLFilePath = path.join(__dirname, 'index.html')
-// const cronJob = new CronJob('0 * * * * *', updateFeed)
+const cronJob = new CronJob('0 * * * * *', updateFeed)
 
 app.use(express.static('assets'))
 
@@ -22,8 +22,7 @@ app.get('/feed', (req, res) => {
 
 app.listen(port, () => console.log(`Server has started`))
 
-// cronJob.start()
-updateFeed()
+cronJob.start()
 
 process.on('unhandledRejection', err => console.error(err))
 process.on('uncaughtException', err => console.error(err))
